b58f36eea0a21cdee4182892758d3e9ba1520900
hsikic1 <hsikic1@etf.unsa.ba>
2018-07-22 18:53:09 +0200

    modifikacija sempliranja uz rub Cobs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,7 +27,7 @@
 
 
 #define delta 0.08
-#define R 20.5959179423
+#define R 10.5959179423
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -38,7 +38,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->customPlot->yAxis->setRange(-5,2*M_PI+0.1);
 
     //weights = {10*0.458333, 10*0.291666, 10*0.166666, 10*0.083333};
-    weights = {4*1.0000, 4*0.6833, 4*0.15835,  4*0.15835};
+    weights = {4*1.0000, 4*0.6833, 4*0.15835, 4*0.15835};
 
     segNumber = 4;
     epsilon = 0.2;
@@ -51,13 +51,13 @@ MainWindow::MainWindow(QWidget *parent) :
     xgoal(3) = 0;
 
     Eigen::VectorXd xinit(segNumber);
-    xinit(0) = M_PI/2;
+    xinit(0) = M_PI;
     xinit(1) = 0;
     xinit(2) = 0;
     xinit(3) = 0;
     segLengths = {2.0, 1.5, 1, 1};
 
-    std::list<std::pair<Eigen::Vector2d, double>> obstacles{std::make_pair(Eigen::Vector2d(-1.6*5,5*sqrt(2)), 1),std::make_pair(Eigen::Vector2d(-1.6*1.4,1.8*sqrt(2)), 1),std::make_pair(Eigen::Vector2d(1.9*std::sqrt(2),1.9*sqrt(2)), 1),std::make_pair(Eigen::Vector2d(1.7*std::sqrt(2),-0.5), 1), std::make_pair(Eigen::Vector2d(5,0), 1.9),std::make_pair(Eigen::Vector2d(0,-2.5), 2)};
+    std::list<std::pair<Eigen::Vector2d, double>> obstacles{std::make_pair(Eigen::Vector2d(-1.6*1.4,1.8*sqrt(2)), 1), std::make_pair(Eigen::Vector2d(1.9*std::sqrt(2),1.9*sqrt(2)), 1),std::make_pair(Eigen::Vector2d(1.7*std::sqrt(2),-0.5), 1), std::make_pair(Eigen::Vector2d(5,0), 1.9)};
     initRRT(xinit, xgoal, obstacles);
 }
 
@@ -157,12 +157,14 @@ bool MainWindow::simpleCollisionCheck(Eigen::Vector2d center, double radius, Eig
     return false;
 }
 
-void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list<std::pair<Eigen::Vector2d , double>> &obstacles){
-    QPen pen;
+void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list<std::pair<Eigen::Vector2d , double>> &obstacles){  
     Eigen::VectorXd T(segNumber);
     std::vector<std::pair<Eigen::VectorXd, rrtNode*>> nodesVector(500, std::make_pair(Eigen::VectorXd(segNumber), nullptr));
-    kdTree *nodes;
-    int missed(0);
+    rrtNode *initRRTN(new rrtNode(xinit, nullptr, 0)), *rrtPtr(nullptr), *rrtNew(nullptr), *dummyNode(nullptr), *lastlastNode;
+    Eigen::VectorXd dummyVector(segNumber), dummyVector2(segNumber), currGoal(segNumber);
+    int missed(0), dominantDimension(100), count(1), lastSize(500);
+    kdTree *nodes;  
+    QPen pen;
 
     for(int i = 0; i < segNumber; i++)
         T(i) = 10*M_PI;
@@ -170,13 +172,7 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
         nodesVector[i].first = T;
     }
 
-    rrtNode *initRRTN(new rrtNode(xinit, nullptr, 0));
-    rrtNode *rrtPtr(nullptr), *rrtNew(nullptr);
     nodesVector[0] = std::make_pair(xinit, initRRTN);
-
-    Eigen::VectorXd dummyVector(segNumber), dummyVector2(segNumber);
-    Eigen::VectorXd currGoal(segNumber);
-    rrtNode *dummyNode(nullptr);
     std::vector<Eigen::VectorXd> vecs;
 
     for(int i = 0; i <= 2 << segNumber; i++){
@@ -192,10 +188,7 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
     currGoal = dummyVector2;
     dummyNode = new rrtNode(dummyVector, nullptr, 0);
     lastNode = dummyNode;
-
-
-    rrtNode *lastlastNode(lastNode);
-    int dominantDimension(100), count(1), lastSize(500);
+    lastlastNode = lastNode;
 
     auto t1 = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < 30000; i++){
@@ -207,7 +200,7 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
             std::mt19937 engine1(seed1);
             std::uniform_real_distribution<double> dist1{lastNode->nodePosition(j), currGoal(j)};
             xrandom(j) = dist1(engine1);
-            if(weightedNorm(xrandom-xgoal, weights.size()) <= epsilon0/4){
+            if((weightedNorm(xrandom-xgoal, weights.size()) <= epsilon0/4)){
                 epsilon = epsilon0/20;
             }
         }
@@ -217,7 +210,7 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
         epsilon = epsilon0;
 
         if ((lastNode == nullptr)){
-            epsilon = 1.5*epsilon0;
+            epsilon = 0.5*epsilon0;
             double minDist(2*M_PI);
             for (int j = 0; j < segNumber; j++){
                 if(weights[j]*std::abs(lastlastNode->nodePosition[j] - xgoal(j)) <= minDist){
@@ -233,10 +226,10 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
 
             lastNode = new rrtNode(ddvv, nullptr, 0);
             for(int j = 0; j < segNumber; j++){
-                lastNode->nodePosition(j) = dummyVector(j);
+                lastNode->nodePosition(j) = dummyVector(j)*weights[j];
                 currGoal(j) = dummyVector2(j);
                 if(j == dominantDimension){
-                    lastNode->nodePosition(j) = lastlastNode->nodePosition(j);
+                    lastNode->nodePosition(j) = lastlastNode->nodePosition(j)*weights[j];
                 }
             }
             missed ++;
@@ -250,18 +243,45 @@ void MainWindow::initRRT(Eigen::VectorXd xinit, Eigen::VectorXd xgoal, std::list
         }
         lastlastNode = lastNode;
     }
+
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
     double duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
     std::cout << "Vrijeme izvršavanja [ms]:" << duration/1000 << std::endl;
     std::cout << "Cobs uzorci: " << missed << std::endl;
 
+    //_______________________________________________________________________________
+    ui->customPlot->addGraph();
+
+    /*for(int i = 0; i < nodesVector.size(); i++){
+        if((nodesVector[i].second->parentNode != nullptr) && nodesVector[i].second != nullptr){
+            auto line = new QCPItemLine(ui->customPlot);
+            line->start->setCoords(nodesVector[i].second->nodePosition[0], nodesVector[i].second->nodePosition[1]);
+            line->end->setCoords(nodesVector[i].second->parentNode->nodePosition[0], nodesVector[i].second->parentNode->nodePosition[1]);
+            line->setHead(QCPLineEnding(QCPLineEnding::esDisc, 3));
+            line->setTail(QCPLineEnding(QCPLineEnding::esDisc, 3));
+            QPen pen;
+            pen.setWidth(2);
+            pen.setColor(QColor(66,131,244));
+            line->setPen(pen);
+        }
+    }*/
     if(rrtPtr == nullptr){
         return;
     }
 
-    //_______________________________________________________________________________
-    ui->customPlot->addGraph();
+    /*while(rrtPtr->parentNode != nullptr){
+        auto line = new QCPItemLine(ui->customPlot);
+        line->start->setCoords(rrtPtr->nodePosition[0], rrtPtr->nodePosition[1]);
+        line->end->setCoords(rrtPtr->parentNode->nodePosition[0], rrtPtr->parentNode->nodePosition[1]);
+        line->setHead(QCPLineEnding(QCPLineEnding::esDisc, 3));
+        line->setTail(QCPLineEnding(QCPLineEnding::esDisc, 3));
+        QPen pen;
+        pen.setWidth(2);
+        pen.setColor(QColor(163, 23, 13));
+        line->setPen(pen);
+        rrtPtr = rrtPtr->parentNode;
+    }*/
 
     while(rrtPtr != nullptr){
         std::vector<double> angles(segNumber, 0);
@@ -337,7 +357,7 @@ rrtNode *MainWindow::extendRRTStar(int &lastsize,int &count, rrtNode *&epsilonNo
     Eigen::VectorXd xepsilon(segNumber);
     kdTreeNode *xmin(nullptr);
     rrtNode *insertedNode(nullptr);
-    double dist(10*M_PI*1.41);
+    double m, dist(20*M_PI*1.41);
 
     epsilonNode = nullptr;
     if((count == 500) ){ ///zamijeniti nekim countom jer je ovaj niz vec 250 i pogledati kdtree build napomenu !!!
@@ -369,7 +389,7 @@ rrtNode *MainWindow::extendRRTStar(int &lastsize,int &count, rrtNode *&epsilonNo
         obstacleIterator++;
     }
 
-    double m = std::min(epsilon, R*std::sqrt(std::log10(count)/((double)(count))));
+    m = std::min(epsilon, R*std::sqrt(std::log10(count)/((double)(count))));
 
     if(count >= 500)
         nodes->rangeSearch(nodes->rootNode, new kdTreeNode(xepsilon,0,nullptr,nullptr,0), nearestNeighbors, m);
@@ -409,7 +429,7 @@ rrtNode *MainWindow::extendRRTStar(int &lastsize,int &count, rrtNode *&epsilonNo
 
     if(count >= 500){
         nodes->insertPoint(nullptr, nodes->rootNode, new kdTreeNode(xepsilon,0,nullptr,insertedNode, 0),insertedNode);
-        //nodesVector.push_back(std::make_pair(insertedNode->nodePosition, insertedNode));
+        nodesVector.push_back(std::make_pair(insertedNode->nodePosition, insertedNode));
     }
     else{
         nodesVector[count] = std::make_pair(insertedNode->nodePosition, insertedNode);
@@ -425,7 +445,7 @@ rrtNode *MainWindow::extendRRTStar(int &lastsize,int &count, rrtNode *&epsilonNo
         nIterator++;
     }
 
-    if(weightedNorm(xepsilon - xgoal, weights.size()) <= epsilon0/50)
+    if(weightedNorm(xepsilon - xgoal, weights.size()) <= epsilon0/40)
         return insertedNode;
 
     epsilonNode = insertedNode;
